ef601666d2ded5f2603003a686820eabdd1ae003
wlals7565 <117993640+wlals7565@users.noreply.github.com>
2024-06-19 19:52:22 +0900

    [UPDATE]: connect database and make some CRUD func

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validationSchema } from './config/validationSchema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -17,6 +19,21 @@ import { ConfigModule } from '@nestjs/config';
           : process.env.NODE_ENV === 'stage'
             ? '.stage.env'
             : '.development.env',
+      validationSchema: validationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 3306),
+        username: configService.get<string>('DATABASE_USERNAME', 'root'),
+        password: configService.get<string>('DATABASE_PASSWORD', ''),
+        database: configService.get<string>('DATABASE_NAME', 'test'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { EmailService } from 'src/email/email.service';
 import * as uuid from 'uuid';
+import { UserEntity } from './entities/user.entity';
+import { DataSource, EntityManager, Repository } from 'typeorm';
+import { ulid } from 'ulid';
 @Injectable()
 export class UsersService {
-  constructor(private emailService: EmailService) {}
+  constructor(
+    private emailService: EmailService,
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>,
+    private dataSource: DataSource,
+  ) {}
 
   async createUser(name: string, email: string, password: string) {
-    await this.checkUserExist(email);
+    const UserExist = await this.checkUserExist(email);
+    if (UserExist) {
+      throw new UnprocessableEntityException(
+        '해당 이메일은 이미 가입되어 있습니다.',
+      );
+    }
 
     const signupVerifyToken = uuid.v1();
 
@@ -26,17 +40,75 @@ export class UsersService {
     throw new Error('Method not implemented');
   }
 
-  private checkUserExist(email: string) {
-    return false;
+  private async checkUserExist(emailAddress: string) {
+    const user = await this.usersRepository.findOne({
+      where: { email: emailAddress },
+    });
+
+    return user !== undefined;
   }
 
-  private saveUser(
+  private async saveUser(
     name: string,
     email: string,
     password: string,
     signupVerifyToken: string,
   ) {
+    const user = new UserEntity();
+    user.id = ulid();
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.signupVerifyToken = signupVerifyToken;
+    await this.usersRepository.save(user);
     return;
+  }
+
+  private async saveUserUsingQueryRunner(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const user = new UserEntity();
+      user.id = ulid();
+      user.name = name;
+      user.email = email;
+      user.password = password;
+      user.signupVerifyToken = signupVerifyToken;
+
+      await queryRunner.manager.save(user);
+
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  private async saveUserUsingTransaction(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    await this.dataSource.transaction(async (manager) => {
+      const user = new UserEntity();
+      user.id = ulid();
+      user.name = name;
+      user.email = email;
+      user.password = password;
+      user.signupVerifyToken = signupVerifyToken;
+
+      await manager.save(user);
+    });
   }
 
   private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
